223f22a5577fd30270415ee3951797d96d5d215b
connieyh <yhc379@nyu.edu>
2015-06-09 18:05:27 -0400

    add one pass method

--- PermutationPalindrome/PalindromePermutation.java
+++ PermutationPalindrome/PalindromePermutation.java
@@ -3,10 +3,8 @@ public class PalindromePermutation {
 	public static boolean isPP(String s) {
 		Map<Character, Integer> map = new HashMap<Character, Integer>();
 		for(int i=0; i<s.length(); i++) {
-			if(!map.containsKey(s.charAt(i))) {
-				map.put(s.charAt(i), 0);
-			}
-			map.put(s.charAt(i), map.get(s.charAt(i))+1);
+			char c = s.charAt(i);
+			map.put(c, map.containsKey(c) ? map.get(s.charAt(i))+1 : 1);
 		}
 		boolean oddSeen = false;
 		for(int i=0; i<s.length(); i++) {
@@ -22,9 +20,26 @@ public class PalindromePermutation {
 		}
 		return true;		
 	}
+
+	public static boolean isPaP(String s) {
+		Set<Character> set = new HashSet<Character>();
+		int count = 0;
+		for(int i=0; i<s.length(); i++) {
+			char c = s.charAt(i);
+			if(!set.contains(c)) {
+				set.add(c);
+				count++;
+			}else {
+				set.remove(c);
+				count--;
+			}
+		}
+		return count == 0 || count == 1 ? true : false;
+	}
+
 	public static void main(String[] args) {
-		System.out.println(isPP("iiicc"));
-		System.out.println(isPP("livci"));
+		System.out.println(isPaP("iiicc"));
+		System.out.println(isPaP("livci"));
 	}
 }
 
---
+++ SortingAlgorithm/QuickSort.java
@@ -0,0 +1,23 @@
+/**
+ @see http://www.algolist.net/Algorithms/Sorting/Quicksort
+*/
+public class QuickSort {
+	public void quickSort(int[] num) {
+
+	}
+}
+
+/*
+	# bucket sort/counting sort -> sort color
+	# three color house
+	  - R(n) = min(G(n-1), B(n-1)) + Rprice[n]
+
+
+*/
+/*
+	1, 12, 5, 26, 7, 14, 3, 7, 2
+	partition(0, 9)
+		i
+
+	n/2 
+*/
