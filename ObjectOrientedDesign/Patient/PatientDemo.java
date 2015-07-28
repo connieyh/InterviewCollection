28e4d3c6f217e8c98c71ef6fd7b246d6da428884
connieyh <yhc379@nyu.edu>
2015-07-28 19:28:49 -0400

    refined

--- ObjectOrientedDesign/Patient/PatientDemo.java
+++ ObjectOrientedDesign/Patient/PatientDemo.java
@@ -1,3 +1,13 @@
+/*
+Design a class which stores demographic information of a person like name, address, phone number, male/female. 
+What if the person has any prefix to the name, multiple phone numbers/addresses, 
+how will you handle dob in case of multiple locations
+(If a person is both at 12 PM in USA on a date, then that date is different from date in India), 
+how will you handle the validations of these fields when the user enters the values
+
+design a class that will be used to store the information of the patients visiting to the hospital
+*/
+
 import java.util.*;
 public class PatientDemo {
 	public static void main(String[] args) {
